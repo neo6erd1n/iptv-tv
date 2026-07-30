88d2ddee03008e8ec91741b061222771723c378d
Николай <nikolai@MacBook-Air-Nikolaj.local>
2026-07-30 19:09:03 +0300

    Исправление загрузки 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.iptvtv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         buildConfigField(
             "String",
